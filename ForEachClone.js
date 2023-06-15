0593b0c468c5984c9a2263020527cd3e3d6dc0c3
Samr Mouna <samr.mouna@gmail.com>
2023-06-15 14:06:20 -0400

    completed and added the 'forEach' clone

---
+++ ForEachClone.js
@@ -0,0 +1,23 @@
+// Declare a test array and print out initial values
+let testArr = [1, 2, 3, 4, 5];
+console.log("Initial values");
+console.log(testArr);
+
+// Make the "myEach" function
+// For each element in the passed array, do ___
+function myEach(arr){
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = callbackDouble(arr[i]);
+    }
+}
+
+// Call back function to add to the custom forEach function
+function callbackDouble(number) {
+    return number * 2;
+}
+
+// Call myEach on our test array
+myEach(testArr);
+// Expected result is each number to be doubled from the original input
+console.log("Expected values: Each value should be double the value in the previous version of the array")
+console.log(testArr);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TTP-Assignment-2
+
+This assignment will recreate some common javascript functions from scratch
